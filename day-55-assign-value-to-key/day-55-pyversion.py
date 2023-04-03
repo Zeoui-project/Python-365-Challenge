dac74bb396995cf9113db9781324d972ce4200ba
Zeoui <revel.project.2022@gmail.com>
2023-04-03 21:12:21 +0700

    adding day 55 folder and code

---
+++ day-55-assign-value-to-key/day-55-pyversion.py
@@ -0,0 +1,16 @@
+# %% [markdown]
+# # Assign a Value to a key in Python
+
+# %%
+d = {'a':1, 'b':2, 'c':3, 'd':4}
+# d is a dictionary
+
+d['c']=3
+# to modify the existing value
+
+d['d']=4
+# to create a key value pair in the dictionary
+
+print(d)
+
+
