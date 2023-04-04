b632d1fec025c7ee07b6fcfa9410c24c5a2255ae
Zeoui <revel.project.2022@gmail.com>
2023-04-04 11:33:41 +0700

    adding day 56 folder and code

---
+++ day-56-screenshot/day-56-pyversion.py
@@ -0,0 +1,8 @@
+# %% [markdown]
+# # Screenshot using Python
+
+# %%
+import pyautogui
+
+myScreenshot = pyautogui.screenshot()
+myScreenshot.save(r'E:\Python\Project-365-Challenge\day-56-screenshot\random.png')
