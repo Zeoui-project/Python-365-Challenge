c4894a170ae22feabc49540d2952468690f25e4e
Zeoui <revel.project.2022@gmail.com>
2023-04-16 13:12:28 +0700

    adding day 67 minor folder and code

---
+++ day-67-add-two-numbers/day-67-pyversion.py
@@ -0,0 +1,20 @@
+# %% [markdown]
+# # Python program to add two numbers
+
+# %%
+# Python program to add two numbers
+
+number1 = input("First number: ")
+number2 = input("Second number: ")
+
+# Adding two numbers
+# User might also enter float numbers
+sum = float(number1) + float(number2)
+
+# Display the sum
+# Print the value in float
+print("The sum of {0} and {1} is {2}".format(number1, number2, sum))
+
+
+
+
