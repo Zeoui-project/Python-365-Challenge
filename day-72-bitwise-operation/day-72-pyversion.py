7d0b6e2f3241bb7327b6dbd051529a007e8aea93
Zeoui <revel.project.2022@gmail.com>
2023-04-21 15:47:58 +0700

    day 72 code and folder

---
+++ day-72-bitwise-operation/day-72-pyversion.py
@@ -0,0 +1,15 @@
+# %% [markdown]
+# # Bitwise Operation on Integer using Python
+
+# %%
+x = int(input("Enter your first number"))
+y = int(input("Enter your second number"))
+
+print("OR Opeartor ", x|y)
+print("AND Operator ", x&y)
+print("XOR Operator ", x^y)
+print("Left shift ", x<<y)
+print("Right shift ", x>>y)
+print("Minus operator ", -x)
+
+
