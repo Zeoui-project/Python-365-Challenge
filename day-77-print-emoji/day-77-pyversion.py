5b97373fddfd476fe0ac3b1ee2392afa31773136
Zeoui <revel.project.2022@gmail.com>
2023-04-26 21:42:18 +0700

    day 77 code and folder

---
+++ day-77-print-emoji/day-77-pyversion.py
@@ -0,0 +1,17 @@
+# %% [markdown]
+# # Python program to print Emojis
+
+# %%
+%pip install emoji
+
+# %%
+import emoji
+
+print(emoji.emojize(":India:"))
+print(emoji.emojize(":books:"))
+print(emoji.emojize(":red_heart:"))
+print(emoji.emojize(":hibiscus:"))
+print(emoji.emojize(":rose:"))
+print(emoji.emojize(":baby:"))
+
+
