6a3093840de9c004ea7ef18c485ee2e7802632ee
Zeoui <revel.project.2022@gmail.com>
2023-04-27 21:04:38 +0900

    day 78 code and folder

---
+++ day-78-image-watermarking/day-78-pyversion.py
@@ -0,0 +1,21 @@
+# %% [markdown]
+# # Image Watermarking using Python
+
+# %%
+from PIL import Image, ImageDraw, ImageFont
+img = Image.open(r'photo.jpg')
+draw = ImageDraw.Draw(img)
+text = "zeoui-project"
+font = ImageFont.truetype('arial.ttf', 50)
+textwidth, textheight = draw.textsize(text, font)
+width, height = img.size
+x = width/2-textwidth/2
+y = height-textheight-50
+draw.text((x,y), text, font=font)
+img.save(r'wmPhoto.png')
+Image.open('wmPhoto.png')
+
+# %%
+
+
+
