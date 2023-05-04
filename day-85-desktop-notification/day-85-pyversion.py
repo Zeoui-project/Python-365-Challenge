1bada902412415ba83646aff3827cfb8375d2df6
Zeoui <revel.project.2022@gmail.com>
2023-05-04 23:37:08 +0900

    day 85 code and folder

---
+++ day-85-desktop-notification/day-85-pyversion.py
@@ -0,0 +1,17 @@
+# %% [markdown]
+# # Desktop Notification using Python
+
+# %%
+import time
+from plyer import notification
+
+if __name__ == '__main__':
+    while True:
+        notification.notify(
+            title = "ALERT!!!",
+            message = "Take a break! It has been an hour",
+            timeout = 10
+        )
+        time.sleep(3600)
+
+
