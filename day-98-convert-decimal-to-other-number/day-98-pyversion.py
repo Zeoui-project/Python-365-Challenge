14d00594bff0e69dcfe888e8b75a6e7614ee9563
Zeoui <revel.project.2022@gmail.com>
2023-05-17 13:25:32 +0900

    day 98 code and folder

---
+++ day-98-convert-decimal-to-other-number/day-98-pyversion.py
@@ -0,0 +1,17 @@
+# %% [markdown]
+# # Converting Decimal to other number using Python
+
+# %%
+# Python program to convert decimal into other number systems
+dec = int(input("Enter a Decimal Number: "))
+
+# Decimal to binary
+print(bin(dec), "in Binary.")
+
+# Decimal to octal
+print(oct(dec), "in Octal.")
+
+# Decimal to Hexadecimal
+print(hex(dec), "in Hexadecimal.")
+
+
