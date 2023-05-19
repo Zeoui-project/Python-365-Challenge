3cb7385582a788e863ac771e7a3cfb3cf52a9aa2
Zeoui <revel.project.2022@gmail.com>
2023-05-19 14:02:02 +0900

    day 100 code and folder

---
+++ day-100-script-to-make-online/day-100-pyversion.py
@@ -0,0 +1,14 @@
+# %% [markdown]
+# # Python script to keep you "ONLINE" all day
+
+# %%
+# Import library
+import pyautogui
+
+# Import time
+import time
+
+# Run the next lines of code while state is set as "True"
+while True:
+    pyautogui.moveRel(0, 10)
+    time.sleep(2)
