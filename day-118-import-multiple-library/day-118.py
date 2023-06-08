09881b9e98c97108de525f14fca91e656d4f67ca
Zeoui <revel.project.2022@gmail.com>
2023-06-08 14:37:51 +0900

    day 118 code and folder

---
+++ day-118-import-multiple-library/day-118.py
@@ -0,0 +1,13 @@
+# %% [markdown]
+# # Importing Multiple library in one line
+
+# %%
+%pip install tensorflow
+
+# %%
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+
+# %%
+import numpy as np, pandas as pd, tensorflow as tf
