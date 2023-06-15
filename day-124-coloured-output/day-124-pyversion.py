2500b96137616c20b90910aaaf77677370a60457
Zeoui <revel.project.2022@gmail.com>
2023-06-15 15:17:00 +0900

    day 124 code and folder

---
+++ day-124-coloured-output/day-124-pyversion.py
@@ -0,0 +1,12 @@
+# %% [markdown]
+# # Coloured output using Python
+
+# %%
+%pip install colorama
+
+# %%
+from colorama import Fore
+print(Fore.RED + "Hello")
+print(Fore.LIGHTRED_EX + "Zeoui")
+print(Fore.YELLOW + "was")
+print(Fore.LIGHTGREEN_EX + "here!")
