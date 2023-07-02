b63b92db02ad25b2f65537eb88f8dcf57b8330dd
Zeoui <revel.project.2022@gmail.com>
2023-07-02 23:43:34 +0900

    update complete code

--- day-141-spider-chart/day-141-pyversion.py
+++ day-141-spider-chart/day-141-pyversion.py
@@ -1,44 +1,38 @@
-# %% [markdown]
-# # Python Program for Spider Chart
-
-# %%
-%pip install matplotlib
-
-# %%
 import matplotlib.pyplot as plt
 import numpy as np
 
-# Data
+#Data
 categories = ['Speed', 'Power', 'Accuracy', 'Endurance', 'Agility']
 player1_stats = [90, 75, 85, 80, 90]
 
-# Number of Categories
+#Number of Categories
 N = len(categories)
 
-# Create an array for the angles
+#Create an array for the angles
 angles = [n / float(N) * 2 * np.pi for n in range(N)]
 angles += angles[:1]
 
-# Initiate the plot
+#Initiate the plot
 fig = plt.figure(figsize=(8, 8))
 ax = fig.add_subplot(111, polar=True)
 
-# Add the first player's stats to the plot
+#Add the first player's stats to the plot
 stats = player1_stats + player1_stats[:1]
 ax.plot(angles, stats, linewidth=2, linestyle='solid', label='Player 1')
 ax.fill(angles, stats, alpha=0.4)
 
-# Set the labels for each axis
-ax.set_thetagrids(np.degrees(angles[:1]), categories)
+#Set the labels for each axis
+ax.set_xticks(angles[:-1])
+ax.set_xticklabels(categories)
 
-# Set the range for each axis
+#Set the range for each axis
 ax.set_ylim(0, 100)
 
-# Add a title
+#Add a title
 plt.title('Spider Chart')
 
-# Add a Legend
+#Add a Legend
 plt.legend(loc='upper right', bbox_to_anchor=(1.3, 1.1))
 
-# show the plot
+#Show the plot
 plt.show()
