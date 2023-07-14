c69472e9ea1f12f42023e81e14847918c7248cbb
Zeoui <revel.project.2022@gmail.com>
2023-07-15 06:47:44 +0900

    day 153 code and folder

---
+++ day-153-realtime-currency-converter/day-153-pyversion.py
@@ -0,0 +1,16 @@
+# %% [markdown]
+# # Realtime Currency Converter using Python
+
+# %%
+%pip install forex_python
+
+# %%
+from forex_python.converter import CurrencyRates
+c = CurrencyRates()
+amount = int(input("Enter the amount: "))
+from_currency = input("From Currency: ").upper()
+to_currency = input("To currency: ").upper()
+
+print(from_currency, " To ", to_currency, amount)
+result = c.convert(from_currency, to_currency, amount)
+print(result)
