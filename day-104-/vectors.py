83887b7f362356867fad7205fe115cbe9914de8d
Zeoui <revel.project.2022@gmail.com>
2023-08-02 09:53:16 +0900

    testing vector 2

--- day-104-/vectors.py
+++ day-104-/vectors.py
@@ -12,16 +12,18 @@ class Vector:
     def __str__(self):
         return f"{self.x}i + {self.y}j + {self.z}k"
 
+    def __getitem__(self, item):
+        if item == 0:
+            return self.x
+        elif item == 1:
+            return self.y
+        elif item == 2:
+            return self.z
+        else:
+            raise IndexError("There are only three elements in the vector")
+
 
 # Testing Vector Class - TO BE DELETED
 test = Vector(3, 5, 9)
-print(test)
-print(repr(test))
 
-test = Vector(2, 2)
-print(test)
-print(repr(test))
-
-test = Vector(y=5, z=3)
-print(test)
-print(repr(test))
+print(test[0])
