6cd0a7a55c84a6d219f1cfbfc7775a6053a2fd0e
Zeoui <revel.project.2022@gmail.com>
2023-08-02 10:42:35 +0900

    testing

--- day-104-/simple_solar_system.py
+++ day-104-/simple_solar_system.py
@@ -1,11 +1,26 @@
 # simple_solar_system.py
 
-from solar_system_3d import SolarSystem, SolarSystemBody
+from solar_system_3d import SolarSystem, Sun, Planet
 
 solar_system = SolarSystem(400)
 
-body = SolarSystemBody(solar_system, 100, velocity=(1, 1, 1))
+sun = Sun(solar_system)
 
-for _ in range(100):
+planets = (
+    Planet(
+        solar_system,
+        position=(150, 50, 0),
+        velocity=(0, 5, 5),
+    ),
+    Planet(
+        solar_system,
+        mass=20,
+        position=(100, -50, 150),
+        velocity=(5, 0, 0)
+    )
+)
+
+while True:
+    solar_system.calculate_all_body_interactions()
     solar_system.update_all()
     solar_system.draw_all()
