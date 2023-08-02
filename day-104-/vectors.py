79b5fed95a73945388f706b12eb0473f99e68f4d
Zeoui <revel.project.2022@gmail.com>
2023-08-02 10:02:28 +0900

    testing vector 3

--- day-104-/vectors.py
+++ day-104-/vectors.py
@@ -36,10 +36,32 @@ class Vector:
             self.z - other.z,
         )
 
+    def __mul__(self, other):
+        if isinstance(other, Vector):  # Vector dot product
+            return (
+                self.x * other.x
+                + self.y * other.y
+                + self.z * other.z
+            )
+        elif isinstance(other, (int, float)):  # Scalar multiplication
+            return Vector(
+                self.x * other,
+                self.y * other,
+                self.z * other,
+            )
+        else:
+            raise TypeError("operand must be Vector, int, or float")
+
+    def __truediv__(self, other):
+        if isinstance(other, (int, float)):
+            return Vector(
+                self.x / other,
+                self.y / other,
+                self.z / other,
+            )
+        else:
+            raise TypeError("operand must be int or float")
+
 # Testing Vector Class - TO BE DELETED
-test = Vector(3, 5, 9) + Vector(1, -3, 2)
-print(test)
-
-
-test = Vector(3, 5, 9) - Vector(1, -3, 2)
+test = Vector(3, 6, 9) / 3
 print(test)
