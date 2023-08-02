01ae83a89167703d22e168e5b766b016262f3e68
Zeoui <revel.project.2022@gmail.com>
2023-08-02 10:22:48 +0900

    testing solar system

--- day-104-/solar_system_3d.py
+++ day-104-/solar_system_3d.py
@@ -5,43 +5,33 @@ import matplotlib.pyplot as plt
 
 from vectors import Vector
 
-# class SolarSystem:
-# ... 
+class SolarSystem:
+    def __init__(self, size):
+        self.size = size
+        self.bodies = []
 
-class SolarSystemBody:
-    min_display_size = 10
-    display_log_base = 1.3
-
-    def __init__(
-        self,
-        solar_system,
-        mass,
-        position=(0, 0, 0),
-        velocity=(0, 0, 0),
-    ):
-        self.solar_system = solar_system
-        self.mass = mass
-        self.position = position
-        self.velocity = Vector(*velocity)
-        self.display_size = max(
-            math.log(self.mass, self.display_log_base),
-            self.min_display_size,
+        self.fig, self.ax = plt.subplots(
+            1,
+            1,
+            subplot_kw={"projection": "3d"},
+            figsize=(self.size / 50, self.size / 50),
         )
-        self.colour = "black"
+        self.fig.tight_layout()
 
-        self.solar_system.add_body(self)
+    def add_body(self, body):
+        self.bodies.append(body)
 
-    def move(self):
-        self.position = (
-            self.position[0] + self.velocity[0],
-            self.position[1] + self.velocity[1],
-            self.position[2] + self.velocity[2],
-        )
+    def update_all(self):
+        for body in self.bodies:
+            body.move()
+            body.draw()
 
-    def draw(self):
-        self.solar_system.ax.plot(
-            *self.position,
-            marker="o",
-            markersize=self.display_size,
-            color=self.colour
-        )
+    def draw_all(self):
+        self.ax.set_xlim((-self.size / 2, self.size / 2))
+        self.ax.set_ylim((-self.size / 2, self.size / 2))
+        self.ax.set_zlim((-self.size / 2, self.size / 2))
+        plt.pause(0.001)
+        self.ax.clear()
+
+# class SolarSystemBody:
+# ...
