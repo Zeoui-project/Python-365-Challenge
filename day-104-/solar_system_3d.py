e7246f33457dc9cae170b58bef0eef7565c75880
Zeoui <revel.project.2022@gmail.com>
2023-08-02 10:19:16 +0900

    testing solar system

--- day-104-/solar_system_3d.py
+++ day-104-/solar_system_3d.py
@@ -1,13 +1,17 @@
 # solar_system_3d.py
 
+import math
 import matplotlib.pyplot as plt
 
 from vectors import Vector
 
 # class SolarSystem:
-# ...  
+# ... 
 
 class SolarSystemBody:
+    min_display_size = 10
+    display_log_base = 1.3
+
     def __init__(
         self,
         solar_system,
@@ -19,5 +23,25 @@ class SolarSystemBody:
         self.mass = mass
         self.position = position
         self.velocity = Vector(*velocity)
+        self.display_size = max(
+            math.log(self.mass, self.display_log_base),
+            self.min_display_size,
+        )
+        self.colour = "black"
 
         self.solar_system.add_body(self)
+
+    def move(self):
+        self.position = (
+            self.position[0] + self.velocity[0],
+            self.position[1] + self.velocity[1],
+            self.position[2] + self.velocity[2],
+        )
+
+    def draw(self):
+        self.solar_system.ax.plot(
+            *self.position,
+            marker="o",
+            markersize=self.display_size,
+            color=self.colour
+        )
