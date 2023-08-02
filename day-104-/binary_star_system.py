2f4e058498b5afbdebc35a7b2b03baf901eeda96
Zeoui <revel.project.2022@gmail.com>
2023-08-02 10:50:26 +0900

    Final Test

--- day-104-/binary_star_system.py
+++ day-104-/binary_star_system.py
@@ -3,6 +3,7 @@
 from solar_system_3d import SolarSystem, Sun, Planet
 
 solar_system = SolarSystem(400)
+# solar_system = SolarSystem(400, projection_2d=True) can turn this ON
 
 suns = (
     Sun(solar_system, position=(40, 40, 40), velocity=(6, 0, 6)),
